a98532686e3c7837c3c80651814cc08a360ff20a
Dmitriy <shalberkind@mail.ru>
2020-11-18 23:53:59 +0300

    Настройка babel

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,16 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
     ],
   },
 };
