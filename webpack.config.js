60da9bb7280f50712deaac31d3e082b28aebb03d
Dmitriy <shalberkind@mail.ru>
2020-11-18 23:22:43 +0300

    Настройка компиляции scss - css

--- webpack.config.js
+++ webpack.config.js
@@ -36,4 +36,12 @@ module.exports = {
       filename: "bundle.[hash].css",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
