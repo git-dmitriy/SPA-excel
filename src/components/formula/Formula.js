0ca0e1e70fe6401ebfde027f96dfac9a913a3e14
Dmitriy <shalberkind@mail.ru>
2020-12-02 20:57:52 +0300

    Шаблон вёрстки компонентов

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,5 +1,10 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 
 export class Formula extends ExcelComponent {
-  
+  static className = "formula";
+  toHTML() {
+    return `
+      <div class="formula__info">fx</div>
+      <div class="formula__input" contenteditable spellcheck="false"></div>`;
+  }
 }
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,5 +1,38 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 
 export class Toolbar extends ExcelComponent {
-  
+  static className = "toolbar";
+
+  toHTML() {
+    return `
+      <div class="toolbar__button">
+        <span class="material-icons">
+          format_bold</span>
+      </div>
+      <div class="toolbar__button">
+        <span class="material-icons">
+          format_italic</span>
+      </div>
+      <div class="toolbar__button">
+        <span class="material-icons">
+          format_underline</span>
+      </div>
+
+      <div class="toolbar__button">
+        <span class="material-icons">
+          format_align_left</span>
+      </div>
+      <div class="toolbar__button">
+        <span class="material-icons">
+          format_align_center</span>
+      </div>
+      <div class="toolbar__button">
+        <span class="material-icons">
+          format_align_right</span>
+      </div>
+      <div class="toolbar__button">
+        <span class="material-icons">
+          format_align_justify</span>
+      </div>`;
+  }
 }
