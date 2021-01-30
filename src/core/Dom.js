031bdb9d29e74531307ab7855328123c32878ea0
Dmitriy <shalberkind@mail.ru>
2021-01-30 23:39:11 +0300

    Add methods for processing DOM elements

--- src/core/Dom.js
+++ src/core/Dom.js
@@ -1,7 +1,41 @@
-class Dom {}
+class Dom {
+  constructor(selector) {
+    this.$el =
+      typeof selector === "string"
+        ? document.querySelector(selector)
+        : selector;
+  }
 
-export function $() {
-  return new Dom();
+  html(html) {
+    if (typeof html === "string") {
+      this.$el.innerHTML = html;
+      return this;
+    }
+    return this.$el.outerHTML.trim();
+  }
+
+  clear() {
+    this.html("");
+    return this;
+  }
+
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el;
+    }
+
+    if (Element.prototype.append) {
+      this.$el.append(node.$el);
+    } else {
+      this.$el.appendChild(node.$el);
+    }
+    return this;
+  }
+}
+
+// event.target
+export function $(selector) {
+  return new Dom(selector);
 }
 
 $.create = (tagName, classes = "") => {
@@ -9,5 +43,5 @@ $.create = (tagName, classes = "") => {
   if (classes) {
     el.classList.add(classes);
   }
-  return el;
+  return $(el);
 };
