2c4aafba7e53f3d2a27f999b46b3c8158cac1a44
Dmitriy <shalberkind@mail.ru>
2021-02-19 23:04:13 +0300

    Preparing a structure for event listeners

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -9,25 +9,19 @@ export class Excel {
   getRoot() {
     const $root = $.create("div", "excel");
 
-    this.components.forEach((Component) => {
-      // const $el = document.createElement("div");
-      // $el.classList.add(Component.className);
-
+    this.component = this.components.map((Component) => {
       const $el = $.create("div", Component.className);
-
       const component = new Component($el);
-      // $el.innerHTML = component.toHTML();
-      // debugger;
       $el.html(component.toHTML());
-
       $root.append($el);
+      return component;
     });
 
     return $root;
   }
 
   render() {
-    // this.$el.insertAdjacentHTML("afterbegin", `<h1>Test</h1>`);
     this.$el.append(this.getRoot());
+    console.log(this.component);
   }
 }
--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,10 +1,19 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 
 export class Formula extends ExcelComponent {
+  constructor($root) {
+    super($root, {
+      name: "Formula",
+      listeners: ["input"],
+    });
+  }
+
   static className = "formula";
   toHTML() {
     return `
       <div class="formula__info">fx</div>
       <div class="formula__input" contenteditable spellcheck="false"></div>`;
   }
+
+  onInput() {}
 }
