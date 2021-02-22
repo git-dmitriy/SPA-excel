306c6adcb4bf72b721097e0afa93f76c739d09c6
Dmitriy <shalberkind@mail.ru>
2021-02-22 23:47:55 +0300

    Display table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,33 +1,35 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
+import { createTable } from "./table.template";
 
 export class Table extends ExcelComponent {
   static className = "table";
 
   toHTML() {
-    return `
-      <div class="table__row">
-        <div class="table__row-info"></div>
-        <div class="table__row-data">
-          <div class="table__column">A</div>
-          <div class="table__column">B</div>
-          <div class="table__column">C</div>
-        </div>
-      </div>
-      <div class="table__row">
-        <div class="table__row-info">1</div>
-        <div class="table__row-data">
-          <div class="table__cell table__cell_selected" contenteditable>A1</div>
-          <div class="table__cell" contenteditable>B1</div>
-          <div class="table__cell" contenteditable>C1</div>
-        </div>
-      </div>
-      <div class="table__row">
-        <div class="table__row-info">2</div>
-        <div class="table__row-data">
-          <div class="table__cell" contenteditable>A2</div>
-          <div class="table__cell" contenteditable>B2</div>
-          <div class="table__cell" contenteditable>C2</div>
-        </div>
-      </div>`;
+    return createTable(31);
   }
 }
+
+// <div class="table__row">
+//   <div class="table__row-info"></div>
+//   <div class="table__row-data">
+//     <div class="table__column">A</div>
+//     <div class="table__column">B</div>
+//     <div class="table__column">C</div>
+//   </div>
+// </div>
+// <div class="table__row">
+//   <div class="table__row-info">1</div>
+//   <div class="table__row-data">
+//     <div class="table__cell table__cell_selected" contenteditable>A1</div>
+//     <div class="table__cell" contenteditable>B1</div>
+//     <div class="table__cell" contenteditable>C1</div>
+//   </div>
+// </div>
+// <div class="table__row">
+//   <div class="table__row-info">2</div>
+//   <div class="table__row-data">
+//     <div class="table__cell" contenteditable>A2</div>
+//     <div class="table__cell" contenteditable>B2</div>
+//     <div class="table__cell" contenteditable>C2</div>
+//   </div>
+// </div>
