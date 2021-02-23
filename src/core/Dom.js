e4ea9c9988c8c204848927682694653079b2e956
Dmitriy <shalberkind@mail.ru>
2021-02-23 23:58:50 +0300

    Create css method for setting styles

--- src/core/Dom.js
+++ src/core/Dom.js
@@ -55,9 +55,14 @@ class Dom {
   findAll(selector) {
     return this.$el.querySelectorAll(selector);
   }
+
+  css(styles = {}) {
+    Object.keys(styles).forEach((key) => {
+      this.$el.style[key] = styles[key];
+    });
+  }
 }
 
-// event.target
 export function $(selector) {
   return new Dom(selector);
 }
