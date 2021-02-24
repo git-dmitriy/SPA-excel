bc75dd0df81f8afae0782d694ed8ccbe8efe9f59
Dmitriy <shalberkind@mail.ru>
2021-02-24 18:36:30 +0300

    Optimize performance

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -18,38 +18,65 @@ export class Table extends ExcelComponent {
   onMousedown(event) {
     if (event.target.dataset.resize) {
       const $resizer = $(event.target);
-      // const parent = $resizer.$el.parentNode;  //!   it's bad practice
-      // const parent = $resizer.$el.closest('.column'); //!  it's bad too.
-
       const $parent = $resizer.closest('[data-type="resizable"]');
       const coords = $parent.getCoords();
-      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
-
       const type = $resizer.data.resize;
+      const sideProp = type === "col" ? "bottom" : "right";
+      const tableRoot = $("." + Table.className);
+      let value;
+      let resizerLength;
+
+      if (type === "col") {
+        resizerLength = Math.floor(tableRoot.getCoords().height);
+      } else {
+        resizerLength = Math.floor(tableRoot.getCoords().width);
+      }
+
+      $resizer.css({
+        [sideProp]: -resizerLength + "px",
+      });
+
       console.log("resize type:", type);
 
       document.onmousemove = (e) => {
+        // console.log("Master, mousemove detected!");
         if (type === "col") {
           console.log("mousemove");
           const delta = e.pageX - coords.right;
-          const value = Math.floor(coords.width + delta);
-          console.log("col value:", value);
-          $parent.css({ width: value + "px" });
-          cells.forEach((el) => (el.style.width = value + "px"));
+          value = Math.floor(coords.width + delta);
+          // console.log("col value:", value);
+
+          $resizer.css({ right: -delta + "px" });
         } else {
           const delta = e.pageY - coords.bottom;
-          const value = Math.floor(coords.height + delta);
-          console.log("row value:", value);
-          $parent.css({ height: value + "px" });
+          value = Math.floor(coords.height + delta);
+          // console.log("row value:", value);
+          $resizer.css({ bottom: -delta + "px" });
         }
       };
 
       document.onmouseup = () => {
+        if (type === "col") {
+          $parent.css({ width: value + "px" });
+          this.$root
+            .findAll(`[data-col="${$parent.data.col}"]`)
+            .forEach((el) => (el.style.width = value + "px"));
+          $resizer.css({ right: "" });
+        } else {
+          // console.log("enter in else block");
+          $parent.css({ height: value + "px" });
+          $resizer.css({ bottom: "" });
+        }
+        $resizer.css({
+          [sideProp]: "",
+        });
+
         document.onmousemove = null;
+        document.onmouseup = null;
       };
 
-      console.log(event.target.parentNode.offsetWidth);
-      console.log("Start resize", event.target.dataset.resize);
+      // console.log(event.target.parentNode.offsetWidth);
+      // console.log("Start resize", event.target.dataset.resize);
     }
   }
 }
