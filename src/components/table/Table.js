f3a9fb0bfb4f736a7ff6d740fa9669417a78fcb1
Dmitriy <shalberkind@mail.ru>
2021-02-26 00:05:38 +0300

    Add selection of multiple cells

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 import { $ } from "../../core/Dom";
+import { matrix } from "../../core/utilities";
 import { resizeHandler } from "./tableResize";
 import { isCell, shouldResize } from "./table.functions";
 import { createTable } from "./table.template";
@@ -34,8 +35,14 @@ export class Table extends ExcelComponent {
       resizeHandler(this.$root, event);
     } else if (isCell(event)) {
       const $target = $(event.target);
-      this.selection.select($target);
-      console.log(event.target);
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current).map((id) =>
+          this.$root.find(`[data-id="${id}"]`)
+        );
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
     }
   }
 }
