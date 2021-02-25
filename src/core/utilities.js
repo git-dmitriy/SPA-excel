81955949dda1704515a0a4e359f39f86c2ceca46
Dmitriy <shalberkind@mail.ru>
2021-02-26 00:04:06 +0300

    Create matrix function

--- src/core/utilities.js
+++ src/core/utilities.js
@@ -1,6 +1,20 @@
+import { range } from "../components/table/table.functions";
+
 export function capitalize(string) {
   if (typeof string !== "string") {
     return "";
   }
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function matrix($target, $current) {
+  const target = $target.id(true);
+  const current = $current.id(true);
+  const cols = range(current.col, target.col);
+  const rows = range(current.row, target.row);
+
+  return cols.reduce((acc, col) => {
+    rows.forEach((row) => acc.push(`${row}:${col}`));
+    return acc;
+  }, []);
 }
