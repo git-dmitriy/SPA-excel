e45a80b5e17309c5d436a74175c39f3cd7da9c94
Dmitriy <shalberkind@mail.ru>
2021-02-25 23:49:10 +0300

    Create range function

--- src/components/table/table.functions.js
+++ src/components/table/table.functions.js
@@ -5,3 +5,10 @@ export function shouldResize(event) {
 export function isCell(event) {
   return event.target.dataset.type === "cell";
 }
+
+export function range(start, end) {
+  if (start > end) {
+    [end, start] = [start, end];
+  }
+  return new Array(end - start + 1).fill("").map((_, index) => start + index);
+}
