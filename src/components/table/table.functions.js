427667af264c7a2e3c1c7da28ee2e5662022ce93
Dmitriy <shalberkind@mail.ru>
2021-02-26 23:48:49 +0300

    Create a function to move through cells
    by pressing keys arrows, Tab and Enter

--- src/components/table/table.functions.js
+++ src/components/table/table.functions.js
@@ -12,3 +12,25 @@ export function range(start, end) {
   }
   return new Array(end - start + 1).fill("").map((_, index) => start + index);
 }
+
+export function nextSelector(key, { col, row }) {
+  const MIN_VALUE = 0;
+  switch (key) {
+  case "ArrowLeft":
+    col = col - 1 < MIN_VALUE ? MIN_VALUE : col - 1;
+    break;
+  case "ArrowUp":
+    row = row - 1 < MIN_VALUE ? MIN_VALUE : row - 1;
+    break;
+  case "ArrowRight":
+  case "Tab":
+    col++;
+    break;
+  case "ArrowDown":
+  case "Enter":
+    row++;
+    break;
+  }
+
+  return `[data-id="${row}:${col}"]`;
+}
