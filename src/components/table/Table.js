ebad05990ace7be64ca1b574e688bf2e412c0f42
Dmitriy <shalberkind@mail.ru>
2021-03-01 23:54:37 +0300

    Create two-way binding

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -12,7 +12,7 @@ export class Table extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: "Table",
-      listeners: ["mousedown", "keydown"],
+      listeners: ["mousedown", "keydown", "input"],
       ...options,
     });
   }
@@ -29,12 +29,19 @@ export class Table extends ExcelComponent {
     super.init();
 
     const $cell = this.$root.find('[data-id="0:0"');
-    this.selection.select($cell);
-
+    this.selectCell($cell);
     this.$on("formula:input", (text) => {
       this.selection.current.text(text);
-      console.log("Formula:input", text);
     });
+
+    this.$on("formula:done", () => {
+      this.selection.current.focus();
+    });
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell);
+    this.$emit("table:select", $cell);
   }
 
   onMousedown(event) {
@@ -71,7 +78,11 @@ export class Table extends ExcelComponent {
       const id = this.selection.current.id(true);
 
       const $next = this.$root.find(nextSelector(key, id));
-      this.selection.select($next);
+      this.selectCell($next);
     }
+  }
+
+  onInput(event) {
+    this.$emit("table:input", $(event.target));
   }
 }
