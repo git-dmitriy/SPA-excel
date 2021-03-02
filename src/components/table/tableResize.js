2a3fe9979a62b631ce8912f24767115cd4ec3132
Dmitriy <shalberkind@mail.ru>
2021-03-02 16:57:17 +0300

    Create columns state

--- src/components/table/tableResize.js
+++ src/components/table/tableResize.js
@@ -2,53 +2,61 @@ import { $ } from "../../core/Dom";
 import { Table } from "./Table";
 
 export function resizeHandler($root, event) {
-  const $resizer = $(event.target);
-  const $parent = $resizer.closest('[data-type="resizable"]');
-  const coords = $parent.getCoords();
-  const type = $resizer.data.resize;
-  const sideProp = type === "col" ? "bottom" : "right";
-  const tableRoot = $("." + Table.className);
-  let value;
-  let resizerLength;
+  return new Promise((resolve) => {
+    const $resizer = $(event.target);
+    const $parent = $resizer.closest('[data-type="resizable"]');
+    const coords = $parent.getCoords();
+    const type = $resizer.data.resize;
+    const sideProp = type === "col" ? "bottom" : "right";
+    const tableRoot = $("." + Table.className);
+    let value;
+    let resizerLength;
 
-  if (type === "col") {
-    resizerLength = Math.floor(tableRoot.getCoords().height);
-  } else {
-    resizerLength = Math.floor(tableRoot.getCoords().width);
-  }
-
-  $resizer.css({
-    [sideProp]: -resizerLength + "px",
-  });
-
-  document.onmousemove = (e) => {
     if (type === "col") {
-      const delta = e.pageX - coords.right;
-      value = Math.floor(coords.width + delta);
-      $resizer.css({ right: -delta + "px" });
+      resizerLength = Math.floor(tableRoot.getCoords().height);
     } else {
-      const delta = e.pageY - coords.bottom;
-      value = Math.floor(coords.height + delta);
-      $resizer.css({ bottom: -delta + "px" });
+      resizerLength = Math.floor(tableRoot.getCoords().width);
     }
-  };
 
-  document.onmouseup = () => {
-    if (type === "col") {
-      $parent.css({ width: value + "px" });
-      $root
-        .findAll(`[data-col="${$parent.data.col}"]`)
-        .forEach((el) => (el.style.width = value + "px"));
-      $resizer.css({ right: "" });
-    } else {
-      $parent.css({ height: value + "px" });
-      $resizer.css({ bottom: "" });
-    }
     $resizer.css({
-      [sideProp]: "",
+      [sideProp]: -resizerLength + "px",
     });
 
-    document.onmousemove = null;
-    document.onmouseup = null;
-  };
+    document.onmousemove = (e) => {
+      if (type === "col") {
+        const delta = e.pageX - coords.right;
+        value = Math.floor(coords.width + delta);
+        $resizer.css({ right: -delta + "px" });
+      } else {
+        const delta = e.pageY - coords.bottom;
+        value = Math.floor(coords.height + delta);
+        $resizer.css({ bottom: -delta + "px" });
+      }
+    };
+
+    document.onmouseup = () => {
+      if (type === "col") {
+        $parent.css({ width: value + "px" });
+        $root
+          .findAll(`[data-col="${$parent.data.col}"]`)
+          .forEach((el) => (el.style.width = value + "px"));
+        $resizer.css({ right: "" });
+      } else {
+        $parent.css({ height: value + "px" });
+        $resizer.css({ bottom: "" });
+      }
+
+      resolve({
+        value,
+        id: type === "col" ? $parent.data.col : null,
+      });
+
+      $resizer.css({
+        [sideProp]: "",
+      });
+
+      document.onmousemove = null;
+      document.onmouseup = null;
+    };
+  });
 }
