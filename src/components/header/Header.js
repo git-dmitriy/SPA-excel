3a90dee23804e7b96dc8c08b0639336514325ac5
Dmitriy <shalberkind@mail.ru>
2021-03-07 21:26:26 +0300

    Optimize storage access

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import { ExcelComponent } from "../../core/ExcelComponent";
 import { $ } from "../../core/Dom";
 import { changeTitle } from "../../redux/actions";
 import { defaultTitle } from "../../constants";
+import { debounce } from "../../core/utilities";
 
 export class Header extends ExcelComponent {
   static className = "header";
@@ -11,6 +12,10 @@ export class Header extends ExcelComponent {
       listeners: ["input"],
       ...options,
     });
+  }
+
+  prepare() {
+    this.onInput = debounce(this.onInput, 300);
   }
 
   toHTML() {
