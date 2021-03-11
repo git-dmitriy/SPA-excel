982207c2c70f5e66963eea584a94ea8f827b919c
Dmitriy <shalberkind@mail.ru>
2021-03-11 21:36:28 +0300

    Create router

---
+++ src/core/routes/Router.js
@@ -0,0 +1,36 @@
+import { $ } from "../Dom";
+import { ActiveRoute } from "./ActiveRoute";
+
+export class Router {
+  constructor(selector, routes) {
+    if (!selector) {
+      throw new Error("Selector is not provided in Router");
+    }
+
+    this.$placeholder = $(selector);
+    this.routes = routes;
+
+    this.changePageHandler = this.changePageHandler.bind(this);
+
+    this.init();
+  }
+
+  init() {
+    window.addEventListener("hashchange", this.changePageHandler);
+    this.changePageHandler();
+  }
+
+  changePageHandler() {
+    console.log(ActiveRoute.path);
+    console.log(ActiveRoute.param);
+
+    const Page = this.routes.excel;
+    const page = new Page();
+    this.$placeholder.append(page.getRoot());
+    page.afterRender();
+  }
+
+  destroy() {
+    window.removeEventListener("hashchange", this.changePageHandler);
+  }
+}
